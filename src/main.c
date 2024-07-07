75178c5f33ece2b29658060f985862f2c57e3969
buvidk <1532493290@qq.com>
2024-07-07 22:41:04 +0800

    只能运行c语言

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+// #include <graphics.h> // 引用图形库头文件
+#include <conio.h>
 #include "module1.h"
 #include "module2.h"
 
@@ -7,6 +9,10 @@ int main()
     printf("Hello, World!\n");
     module1_function();
     module2_function();
+    // initgraph(640,480,0);   // 创建绘图窗口，大小为 640x480 像素
+    // circle(200, 200, 100); // 画圆，圆心(200, 200)，半径 100
+    // _getch();              // 按任意键继续
+    // closegraph();
     getchar();
     return 0;
 }
